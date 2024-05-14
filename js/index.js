712e776d0f38f2019847edcecab05bf17d99a7c3
JoshuaFigura <joshua.figura@gmail.com>
2024-05-14 14:00:19 -0400

    Fixedddd

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 console.log("your indexedDB.js file is loaded correctly");
+
+<script>
+function mouseOver(element)
+{
+    element.style.color = 'red';
+}
+
+function mouseOut(element)
+{
+    element.style.color = 'black';
+}
+</script>
